564f8172dae851263b6536ad3fd58c3cdbbe492a
nawress <nawressel761@gmail.com>
2025-03-16 12:48:01 +0100

    formulaire frontend

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import FormulairePartenaire from './Components/FormulairePartenaire/FormulairePartenaire';
+import FormulaireStagiaire from './Components/FormulaireStagiaire/FormulaireStagiaire';
+import FormulaireStartup from './Components/FormulaireStartup/FormulaireStartup';
+
+function App() {
+  return (
+    <div >
+<FormulaireStagiaire />
+<FormulairePartenaire />
+<FormulaireStartup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FormulaireStartup/FormulaireStartup.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { FaFacebook, FaGoogle, FaLinkedin, FaLock, FaPhone, FaTwitter, FaUser } from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { TfiAgenda } from "react-icons/tfi";
+import "./FormulaireStartup.css";
+
+const FormulaireStartup = () => {
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
+
+  return (
+    <div className={`container ${isSignUpMode ? "sign-up-mode" : ""}`}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="" className="sign-in-form">
+            <h2 className="title">Login</h2>
+            <div className="input-field">
+              <MdEmail className="icon" />
+              <input type="email" placeholder="Enter your email" />
+            </div>
+            <div className="input-field">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Enter your password" />
+            </div>
+            <input type="submit" value="Login" className="btn solid" />
+            <p className="social-text">Or Sign in with social platforms</p>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <FaFacebook />
+              </a>
+              <a href="#" className="social-icon">
+                <FaTwitter />
+              </a>
+              <a href="#" className="social-icon">
+                <FaGoogle />
+              </a>
+              <a href="#" className="social-icon">
+                <FaLinkedin />
+              </a>
+            </div>
+          </form>
+
+          <form action="" className="sign-up-form">
+            <h2 className="title">Formulaire Startup</h2>
+            <div className="input-field">
+            <FaUser className="icon" />
+              <input type="text" placeholder="Enter the name of society" />
+            </div>
+            <div className="input-field">
+            <FaLocationPin className="icon"/>
+              <input type="text" placeholder="Enter the address of society" />
+            </div>
+            <div className="input-field">
+            <FaPhone className="icon"/>
+              <input type="text" placeholder="Enter the phone of society" />
+            </div>
+            <div className="input-field">
+            <MdEmail className="icon" />
+              <input type="email" placeholder="Enter they email of society" />
+            </div>
+            <div className="input-field">
+            <TfiAgenda className='icon'/>
+                <input type="date" placeholder="Enter the date of creation" />
+                
+            </div>
+            <div className="input-field">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Enter your password" />
+            </div>
+            <input type="submit" value="Sign up" className="btn solid" />
+            <p className="social-text">Or Sign up with social platforms</p>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <FaFacebook />
+              </a>
+              <a href="#" className="social-icon">
+                <FaTwitter />
+              </a>
+              <a href="#" className="social-icon">
+                <FaGoogle />
+              </a>
+              <a href="#" className="social-icon">
+                <FaLinkedin />
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>New here?</h3>
+            <p>Lorem......</p>
+            <button className="btn transparent" onClick={() => setIsSignUpMode(true)}>
+              Sign up
+            </button>
+          </div>
+          <img src="/Assets/1.png" className="image" alt="Usability Testing" />
+        </div>
+
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>One of us ?</h3>
+            <p>Lorem....</p>
+            <button className="btn transparent" onClick={() => setIsSignUpMode(false)}>
+              Sign in
+            </button>
+          </div>
+          <img src="/Assets/1.png" className="image" alt="Usability Testing" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormulaireStartup;
